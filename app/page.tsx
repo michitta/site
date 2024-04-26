9c6f102be5b848ce4b646b4d8ba1ee6d457fea49
michi (｡•̀ᴗ-)✧ <admjgptw@yandex.ru>
2024-04-27 01:11:45 +0300

    switch to mdx (initial)

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,10 @@
-'use client';
+import Index from "./index.mdx";
+import styles from "./page.module.scss"
 
-import { useEffect } from "react";
-import styles from "./page.module.scss";
-import { useRouter } from "next/navigation";
-
-export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.push('/about')
-  }, [])
-  return (
-    <main className={styles.page}>
-    </main>
-  );
+export default function Page() {
+    return (
+        <main className={styles.page}>
+            <Index />
+        </main>
+    )
 }
---
+++ next.dynamic.ts
@@ -0,0 +1,35 @@
+import { existsSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { glob } from "glob";
+import { join, sep } from "node:path";
+
+// Была хорошая разработка, но ггвп
+
+export const cachedMardownFiles = new Map();
+
+export const getMDXRoutes = async () => {
+  const whiteList = ["index.md", "index.mdx"];
+  // Создаём Map, который будет жёстко хавать путь к файлу и название файла
+  const files = await glob("**/*.{md,mdx}", {
+    root: process.cwd(),
+    cwd: "pages",
+    posix: true,
+  });
+
+  files.forEach(async (fileName) => {
+    let pathName = fileName.replace(/.(mdx|md)?$/i, "");
+    if (!whiteList.includes(fileName))
+      pathName = fileName.replace(/((\/)?(index))?\.(mdx|md)?$/i, "");
+
+    if (pathName.length > 1 && pathName.endsWith(sep)) {
+      pathName = pathName.substring(0, pathName.length - 1);
+    }
+
+    let filePath = join(process.cwd(), "pages", fileName);
+
+    if (existsSync(filePath)) {
+      const content = await readFile(filePath, "utf8");
+      cachedMardownFiles.set(pathName, content);
+    }
+  });
+};
